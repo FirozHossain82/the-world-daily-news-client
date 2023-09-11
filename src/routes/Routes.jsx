c19717f4413834a30d394c8a5110d7ee3152dd30
FirozHossain82 <firozhossain295@gmail.com>
2023-09-11 12:17:20 +0600

    home section added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "../layouts/Main.jsx";
 import Home from "../pages/Home/Home/Home.jsx";
+import Category from "../pages/Home/Category/Category.jsx";
+import NewsLayout from "../layouts/NewsLayout.jsx";
+import News from "../pages/News/News/News.jsx";
 
 
 const router = createBrowserRouter([
@@ -11,6 +14,21 @@ const router = createBrowserRouter([
             {
                 path:'/',
                 element:<Home></Home>
+            },
+            {
+               path:'/category/:id', 
+               element:<Category></Category>,
+               loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
+            }
+        ]
+    },
+    {
+        path:'/news',
+        element:<NewsLayout></NewsLayout>,
+        children:[
+            {
+                path:'/news/:id',
+                element:<News></News>
             }
         ]
     }
